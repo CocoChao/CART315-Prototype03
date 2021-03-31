59d8a955cdc0075991f4e19ba4879a686073786a
Carole Chao <carolechao@hotmail.com>
2021-03-30 23:24:15 -0400

    Prototype3: Remove end game and game manager script+object

--- Assets/Scripts/ScoreScript.cs
+++ Assets/Scripts/ScoreScript.cs
@@ -9,9 +9,6 @@ public class ScoreScript : MonoBehaviour
     public static int scoreValue = 0;
     Text score;
 
-    //bool gameHasEnded = false;
-    //public float nextLevelDelay = 2f;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -24,17 +21,4 @@ public class ScoreScript : MonoBehaviour
         score.text = "Rocks collected: " + scoreValue + "/10";
     }
 
-    //public void EndGame()
-    //{
-    //    if (gameHasEnded == false)
-    //    {
-    //        gameHasEnded = true;
-    //        Debug.Log("GAME OVER");
-    //        Invoke("nextLevel", nextLevelDelay);
-    //    }
-    //}
-    //void nextLevel()
-    //{
-    //    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    //}
 }
